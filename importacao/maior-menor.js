506cc25b44fa5d5fda92008930d322ddc435f9d6
Emerson78costad <emersoncostadantas1995@gmail.com>
2023-05-24 15:59:02 -0400

    finished

--- importacao/maior-menor.js
+++ importacao/maior-menor.js
@@ -5,6 +5,26 @@ for (let i = 0; i < 10; i++) {
     let numero = Math.floor(Math.random() * 100)
     numeroArray.push(numero)
 }
-console.log(numeroArray);
  
- 
+const MaiorMenor = function (numero) {
+    let maior=0;
+    let menor=0;
+    if (numero>maior) {
+        maior = numero;
+        menor = maior;
+    }
+    if (numero > maior){
+        maior = numero;
+    }     
+    if (numero < menor){
+        menor = numero;
+        
+    }
+    return menor, maior;
+}
+for (let i = 0; i < numeroArray.length; i++) {
+   console.log(MaiorMenor(numeroArray[i]))
+}
+
+
+   
